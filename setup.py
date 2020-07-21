fcd64838e4fb0ff2f2642f9a737b9d87e12209f1
Tony Angerilli <tony@7geese.com>
2020-07-21 09:52:45 -0700

    drop the version requirement entirely

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     package_dir={'': 'src'},
     install_requires=[
         'Django>=1.11.13',
-        'python-dateutil<2.7',
+        'python-dateutil',
         'scim2-filter-parser==0.3.4',
     ],
     tests_require=[
